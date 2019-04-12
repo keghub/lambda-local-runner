069171fb86d83b2528d649c639b65ff58446af01
Renato Golia <renato.golia@educations.com>
2019-04-12 14:39:28 +0200

    Removed skipped tests

--- tests/Tests.Lambda.LocalRunner/LambdaRunnerTests.cs
+++ tests/Tests.Lambda.LocalRunner/LambdaRunnerTests.cs
@@ -20,50 +20,50 @@ namespace Tests
             clause.Verify(typeof(LambdaRunner).GetConstructors());
         }
 
-        [Test, AutoMoqData]
-        [Ignore("Not stopping")]
-        public async Task IWebHost_can_be_programmatically_stopped(
-            IWebHost host,
-            IHostingEnvironment hostingEnvironment,
-            IServerAddressesFeature serverAddressesFeature,
-            IApplicationLifetime applicationLifetime
-        )
-        {
-            Mock.Get(host.Services).Setup(s => s.GetService(typeof(IHostingEnvironment))).Returns(hostingEnvironment);
-            Mock.Get(host.Services).Setup(s => s.GetService(typeof(IApplicationLifetime))).Returns(applicationLifetime);
-            Mock.Get(host.ServerFeatures).Setup(p => p.Get<IServerAddressesFeature>()).Returns(serverAddressesFeature);
+        // [Test, AutoMoqData]
+        // [Ignore("Not stopping")]
+        // public async Task IWebHost_can_be_programmatically_stopped(
+        //     IWebHost host,
+        //     IHostingEnvironment hostingEnvironment,
+        //     IServerAddressesFeature serverAddressesFeature,
+        //     IApplicationLifetime applicationLifetime
+        // )
+        // {
+        //     Mock.Get(host.Services).Setup(s => s.GetService(typeof(IHostingEnvironment))).Returns(hostingEnvironment);
+        //     Mock.Get(host.Services).Setup(s => s.GetService(typeof(IApplicationLifetime))).Returns(applicationLifetime);
+        //     Mock.Get(host.ServerFeatures).Setup(p => p.Get<IServerAddressesFeature>()).Returns(serverAddressesFeature);
 
-            var cts = new CancellationTokenSource();
+        //     var cts = new CancellationTokenSource();
 
-            cts.CancelAfter(TimeSpan.FromSeconds(1));
+        //     cts.CancelAfter(TimeSpan.FromSeconds(1));
 
-            await host.RunAsync(cts.Token);
+        //     await host.RunAsync(cts.Token);
 
-            Mock.Get(host).Verify(p => p.StartAsync(It.IsAny<CancellationToken>()));
-        }
+        //     Mock.Get(host).Verify(p => p.StartAsync(It.IsAny<CancellationToken>()));
+        // }
 
-        [Test, AutoMoqData]
-        [Ignore("Not stopping")]
-        public async Task RunAsync_uses_StartAsync(
-            IWebHost host,
-            IHostingEnvironment hostingEnvironment,
-            IServerAddressesFeature serverAddressesFeature,
-            IApplicationLifetime applicationLifetime
-        )
-        {
-            var sut = new LambdaRunner(host);
-            Mock.Get(host.Services).Setup(s => s.GetService(typeof(IHostingEnvironment))).Returns(hostingEnvironment);
-            Mock.Get(host.Services).Setup(s => s.GetService(typeof(IApplicationLifetime))).Returns(applicationLifetime);
-            Mock.Get(host.ServerFeatures).Setup(p => p.Get<IServerAddressesFeature>()).Returns(serverAddressesFeature);
+        // [Test, AutoMoqData]
+        // [Ignore("Not stopping")]
+        // public async Task RunAsync_uses_StartAsync(
+        //     IWebHost host,
+        //     IHostingEnvironment hostingEnvironment,
+        //     IServerAddressesFeature serverAddressesFeature,
+        //     IApplicationLifetime applicationLifetime
+        // )
+        // {
+        //     var sut = new LambdaRunner(host);
+        //     Mock.Get(host.Services).Setup(s => s.GetService(typeof(IHostingEnvironment))).Returns(hostingEnvironment);
+        //     Mock.Get(host.Services).Setup(s => s.GetService(typeof(IApplicationLifetime))).Returns(applicationLifetime);
+        //     Mock.Get(host.ServerFeatures).Setup(p => p.Get<IServerAddressesFeature>()).Returns(serverAddressesFeature);
 
-            var cts = new CancellationTokenSource();
+        //     var cts = new CancellationTokenSource();
 
-            cts.CancelAfter(TimeSpan.FromSeconds(1));
+        //     cts.CancelAfter(TimeSpan.FromSeconds(1));
 
-            await sut.RunAsync(cts.Token);
+        //     await sut.RunAsync(cts.Token);
 
-            Mock.Get(host).Verify(p => p.StartAsync(It.IsAny<CancellationToken>()));
-        }
+        //     Mock.Get(host).Verify(p => p.StartAsync(It.IsAny<CancellationToken>()));
+        // }
 
         [Test]
         public void Create_returns_a_builder()
